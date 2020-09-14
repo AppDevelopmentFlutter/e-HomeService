3475defcdc787deea813b8817e00c90295ab04ba
swathiacharya <acharyaswathi999@gmail.com>
2020-09-14 14:39:22 +0530

    appBar is commented

--- eHomeService/lib/chat/views/chatrooms.dart
+++ eHomeService/lib/chat/views/chatrooms.dart
@@ -58,26 +58,26 @@ class _ChatRoomState extends State<ChatRoom> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          "assets/logo.png",
-          height: 40,
-        ),
-        elevation: 0.0,
-        centerTitle: false,
-        actions: [
-          GestureDetector(
-            onTap: () {
-              AuthService().signOut();
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) => Authenticate()));
-            },
-            child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: Icon(Icons.exit_to_app)),
-          )
-        ],
-      ),
+      // appBar: AppBar(
+      //   title: Image.asset(
+      //     "assets/logo.png",
+      //     height: 40,
+      //   ),
+      //   elevation: 0.0,
+      //   centerTitle: false,
+      //   actions: [
+      //     GestureDetector(
+      //       onTap: () {
+      //         AuthService().signOut();
+      //         Navigator.pushReplacement(context,
+      //             MaterialPageRoute(builder: (context) => Authenticate()));
+      //       },
+      //       child: Container(
+      //           padding: EdgeInsets.symmetric(horizontal: 16),
+      //           child: Icon(Icons.exit_to_app)),
+      //     )
+      //   ],
+      // ),
       body: Container(
         child: chatRoomsList(),
       ),
